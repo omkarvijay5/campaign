24e5e7c242a9dfe09a25162d6a220c002adccc1d
Omkar Vijay <omkarvijay5@gmail.com>
2018-07-02 13:09:08 +0530

    create basic template and render the / as a main dashboard view

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 from core import views
 
 urlpatterns = [
+    url('', views.Dashboard.as_view(), name='dashboard'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+# python imports
 
-# Create your views here.
+# django imports
+from django.shortcuts import render
+from django.views.generic import View
+
+# third party imports
+
+
+class Dashboard(View):
+    """
+    Dashboard is the main dashboard of the campaign
+    Dashboard has multiple forms which enables to create a campaign
+    """
+    template_name = 'core/dashboard.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {})
